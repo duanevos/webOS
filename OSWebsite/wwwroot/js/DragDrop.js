cc95fda101d0b32e83e4d83046696250ffd957bd
vosvosvos <indvfx@gmail.com>
2022-11-27 17:18:46 +0100

    Drag and drop windows

---
+++ OSWebsite/wwwroot/js/DragDrop.js
@@ -0,0 +1,17 @@
+﻿const position = { x: 0, y: 0 }
+
+interact('.draggable').draggable({
+    cursorChecker: function () { return false; },
+    allowFrom: '#header',
+    
+    
+    listeners: {
+        move(event) {
+            position.x += event.dx
+            position.y += event.dy
+
+            event.target.style.transform =
+                `translate(${position.x}px, ${position.y}px)`
+        },
+    }
+})
