2c7a6641c11fe531cc9122e53db4df0eb68c6f60
vosvosvos <indvfx@gmail.com>
2022-11-28 21:08:28 +0100

    Dragging windows, list of active applications

--- OSWebsite/wwwroot/js/DragDrop.js
+++ OSWebsite/wwwroot/js/DragDrop.js
@@ -1,17 +1,3 @@
-﻿var position = { x: 0, y: 0 }
-
-interact('.draggable').draggable({
-    cursorChecker: function () { return false; },
-    allowFrom: '#header',
-    
-    listeners: {
-        move(event) {
-            position.x += event.dx
-            position.y += event.dy
-
-            event.target.style.transform =
-                `translate(${position.x}px, ${position.y}px)`
-        },
-    }
-})
-
+﻿function draggable() {
+      $(".draggable").draggable();
+}
